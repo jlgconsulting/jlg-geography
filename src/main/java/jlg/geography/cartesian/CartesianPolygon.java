8610b7524af094eaf67b45c6bec8119946c616a5
Dan Geabunea <dan.geabunea@jlg.ro>
2015-05-12 17:50:21 +0300

    minor refactorings

---
+++ src/main/java/jlg/geography/cartesian/CartesianPolygon.java
@@ -0,0 +1,73 @@
+package jlg.geography.cartesian;
+
+import jlg.codecontract.CodeContractException;
+import jlg.geography.AbstractPolygon;
+import jlg.geography.BoundingBox;
+
+public class CartesianPolygon extends AbstractPolygon<Cartesian>{
+    public CartesianPolygon(Cartesian[] points){
+        super(points);
+        checkIfPolygonIsClosed();
+    }
+
+    public CartesianPolygon(int[] coordinates){
+        super();
+        for(int i=0; i<coordinates.length;i+=2){
+            this.points.add(new Cartesian(coordinates[i],coordinates[i+1]));
+        }
+        checkIfPolygonIsClosed();
+    }
+
+    @Override
+    public BoundingBox<Cartesian> getBoundingBox() {
+        return new CartesianBoundingBox(points.toArray(new Cartesian[points.size()]));
+    }
+
+    @Override
+    public boolean contains(Cartesian cartesian) {
+        if(!cartesian.isInBoundingBox(getBoundingBox())){
+            return false;
+        }
+
+        int windingNumber = 0;
+        for(int i=0; i<this.points.size()-1;i++){
+            if(this.points.get(i).getY() <= cartesian.getY()){
+                if(this.points.get(i+1).getY() > cartesian.getY()){
+                    //an upward crossing
+                    if(isLeft(this.points.get(i), this.points.get(i+1), cartesian) > 0){
+                        //has a valid up intersect
+                        ++windingNumber;
+                    }
+                }
+            }
+            else{
+                if(this.points.get(i+1).getY() <= cartesian.getY()){
+                    //a downward crossing
+                    if(isLeft(this.points.get(i), this.points.get(i+1), cartesian) < 0){
+                        //has a valid down intersect
+                        --windingNumber;
+                    }
+                }
+            }
+        }
+        return windingNumber != 0;
+    }
+
+    private int isLeft(Cartesian p0, Cartesian p1, Cartesian somePoint){
+        return (p1.getX() - p0.getX()) * (somePoint.getY() - p0.getY())
+                - (somePoint.getX() - p0.getX()) * (p1.getY() - p0.getY());
+    }
+
+    private void checkIfPolygonIsClosed() {
+        Cartesian firstPolygonPoint = this.points.get(0);
+        Cartesian lastPolygonPoint = this.points.get(this.points.size()-1);
+
+        if(firstPolygonPoint.getX() != lastPolygonPoint.getX()){
+            throw new CodeContractException("The polygon is not closed. Can not construct the polygon. Latitudes are different. ");
+        }
+
+        if(firstPolygonPoint.getY() != lastPolygonPoint.getY()){
+            throw new CodeContractException("The polygon is not closed. Can not construct the polygon. Longitudes are different");
+        }
+    }
+}
---
+++ src/test/java/jlg/geography/test/CartesianPolygonPointTest.java
@@ -0,0 +1,123 @@
+package jlg.geography.test;
+
+import jlg.codecontract.CodeContractException;
+import jlg.geography.BoundingBox;
+import jlg.geography.cartesian.Cartesian;
+import jlg.geography.cartesian.CartesianBoundingBox;
+import jlg.geography.cartesian.CartesianPolygon;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class CartesianPolygonPointTest {
+
+    @Test
+    public void the_constructor_with_points_should_build_polygon() {
+        //arrange
+        Cartesian[] points = {
+                new Cartesian(1, 1),
+                new Cartesian(2, 2),
+                new Cartesian(1, 1)
+        };
+
+        //act
+        CartesianPolygon polygon = new CartesianPolygon(points);
+
+        //assert
+        assertEquals(3, polygon.getPoints().size());
+    }
+
+    @Test(expected = CodeContractException.class)
+    public void the_constructor_with_points_that_do_no_close_should_throw() {
+        //arrange
+        Cartesian[] points = {
+                new Cartesian(1, 1),
+                new Cartesian(2, 2),
+                new Cartesian(3, 3)
+        };
+
+        //act
+        CartesianPolygon polygon = new CartesianPolygon(points);
+    }
+
+    @Test
+    public void the_constructor_with_coordinates_should_build_polygon() {
+        //arrange
+        int[] coordinates = {1, 1, 2, 2, 1, 1};
+
+        //act
+        CartesianPolygon polygon = new CartesianPolygon(coordinates);
+
+        //assert
+        assertEquals(3, polygon.getPoints().size());
+    }
+
+    @Test(expected = CodeContractException.class)
+    public void the_constructor_with_coordinates_that_do_not_close_should_throw() {
+        //arrange
+        int[] coordinates = {1, 1, 2, 2, 3, 3};
+
+        //act
+        CartesianPolygon polygon = new CartesianPolygon(coordinates);
+    }
+
+    @Test
+    public void the_get_bounding_box_should_return_correct_bounding_box() {
+        //arrange
+        int[] coordinates = {1, 1, 2, 2, 1, 1};
+
+        //act
+        CartesianPolygon polygon = new CartesianPolygon(coordinates);
+
+        //assert
+        BoundingBox<Cartesian> boundingBox = polygon.getBoundingBox();
+        assertEquals(1, boundingBox.getMinCoordinate().getX());
+        assertEquals(1, boundingBox.getMinCoordinate().getY());
+        assertEquals(2, boundingBox.getMaxCoordinate().getX());
+        assertEquals(2, boundingBox.getMaxCoordinate().getY());
+    }
+
+    @Test
+    public void the_contains_point_method_when_point_inside_should_return_true() {
+        //arrange
+        Cartesian[] points = {
+                new Cartesian(1,1),
+                new Cartesian(4,1),
+                new Cartesian(4,3),
+                new Cartesian(1,3),
+                new Cartesian(1,1)
+        };
+
+        CartesianPolygon polygon = new CartesianPolygon(points);
+        Cartesian pointInside = new Cartesian(2,2);
+
+        //act
+        boolean result = polygon.contains(pointInside);
+
+        //assert
+        assertTrue(result);
+    }
+
+    @Test
+    public void the_contains_point_method_when_point_outside_should_return_false() {
+        //arrange
+        Cartesian[] points = {
+                new Cartesian(1,1),
+                new Cartesian(4,1),
+                new Cartesian(4,3),
+                new Cartesian(1,3),
+                new Cartesian(1,1)
+        };
+
+        CartesianPolygon polygon = new CartesianPolygon(points);
+        Cartesian pointOutside = new Cartesian(4,4);
+
+        //act
+        boolean result = polygon.contains(pointOutside);
+
+        //assert
+        assertFalse(result);
+    }
+}
